cf992ad0f04e9092826cf6d614cc77effadf18e4
Phanyapat Chalermchai <kayzazz1@hotmail.com>
2021-03-31 16:27:26 +0700

    Enter

--- src/app/my-form/my-form.component.ts
+++ src/app/my-form/my-form.component.ts
@@ -9,10 +9,10 @@ export class MyFormComponent implements OnInit {
 form: FormGroup;
 constructor(private fb: FormBuilder) {
   this.form = this.fb.group({
-  phoneNo: ['', [Validators.required, Validators.minLength(10)]],
-  email: ['', Validators.email],
-  password: ['', [Validators.required, Validators.minLength(8)]],
-  confirmPassword: ['', Validators.required]
+  name: ['', [Validators.required]],
+  email: ['', [Validators.required, Validators.email]],
+  postaddress: ['', [Validators.required, Validators.minLength(8)]],
+  comments: ['']
   });
 }
  
